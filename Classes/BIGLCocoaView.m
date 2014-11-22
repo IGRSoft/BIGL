66eafee7850e364f15150c6fc0f028aabd95af2f
iKorich <korich.vi.p@gmail.com>
2014-11-22 21:44:02 +0200

    Updated 64-bit support

--- Classes/BIGLCocoaView.m
+++ Classes/BIGLCocoaView.m
@@ -1,43 +1,69 @@
-//
-//  BIGLCocoaView.m
-//  BIGL
-//
-//  Created by mick on Thu Jul 08 2004.
-//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
-//
+/*
+		File:			BIGLCocoaView.m
+		Program:		binaervarianz OpenGL Framework
+		Author:			Michael Rossberg
+						mick@binaervarianz.de
+		Changes:        Vitalii Parovishnyk (Korich) 22.11.2014
+						http://igrsoft.com
+ 
+	Description:	This framework provides abstract Cocoa methods to quickly draw 2D sprites
+ 
+ This file is part of BIGL.
+ 
+ BIGL is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ BIGL is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with BIGL; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #import "BIGLCocoaView.h"
 #import "BIGLSubView.h"
 
 @implementation BIGLCocoaView
 
-- (id)initWithFrame:(NSRect)frame {
-    self = [super initWithFrame:frame];
-    if (self) {
-        _color = [NSColor blackColor];
-    }
-    return self;
+- (instancetype)initWithFrame:(NSRect)frame
+{
+	self = [super initWithFrame:frame];
+	if (self)
+	{
+		_color = [NSColor blackColor];
+	}
+	
+	return self;
 }
 
-- (void)setSubViews:(NSArray*)subs {
-    NSParameterAssert(subs);
-    
-    _subs = subs;
-}
-- (void)setBackgroundColor:(NSColor*)color {
-    NSParameterAssert(color);
-    
-    _color = color;
+- (void)setSubViews:(NSArray*)subs
+{
+	NSParameterAssert(subs);
+	
+	_subs = subs;
 }
 
-- (void)drawRect:(NSRect)rect {
-    int i;
-    [_color set];
-    NSRectFill(rect);
+- (void)setBackgroundColor:(NSColor*)color
+{
+	NSParameterAssert(color);
+	
+	_color = color;
+}
 
-    for (i = [_subs count]; i > 0; i--) {
-        [(BIGLSubView*)_subs[i-1] drawCocoaAtPoint:NSZeroPoint];
-    }
+- (void)drawRect:(NSRect)rect
+{
+	[_color set];
+	NSRectFill(rect);
+	
+	for (NSInteger i = [_subs count]; i > 0; --i)
+	{
+		[(BIGLSubView*)_subs[i-1] drawCocoaAtPoint:NSZeroPoint];
+	}
 }
 
 #pragma mark -
--- Classes/BIGLTextView.h
+++ Classes/BIGLTextView.h
@@ -1,44 +1,48 @@
 /*
         File:			BIGLTextView.h
-        Program:		binaervarianz OpenGL Framework
-	Author:			Michael Roßberg
-				mick@binaervarianz.de
-	Description:		This framework provides abstract Cocoa methods to quickly draw 2D sprites
-                
-        This file is part of BIGL.
-
-    BIGL is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    BIGL is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with BIGL; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+		Program:		binaervarianz OpenGL Framework
+		Author:			Michael Rossberg
+						mick@binaervarianz.de
+		Changes:        Vitalii Parovishnyk (Korich) 22.11.2014
+						http://igrsoft.com
+ 
+	Description:	This framework provides abstract Cocoa methods to quickly draw 2D sprites
+ 
+ This file is part of BIGL.
+ 
+ BIGL is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ BIGL is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with BIGL; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #import "BIGLSubView.h"
 #import <OpenGL/glext.h>
 #import <OpenGL/OpenGL.h>
 #import <OpenGL/CGLContext.h>
 
-@interface BIGLTextView : BIGLSubView {
-    CGLContextObj       _cgl_ctx;       // current context at time of texture creation
-    GLuint              _texName;
-    NSAttributedString  *_string;
-    NSColor             *_color, *_borderColor;
-    NSSize              _texSize;
-    NSSize              _marginSize;    // offset default is 4 width 2 height
-    NSSize              _frameSize;
+@interface BIGLTextView : BIGLSubView
+{
+	CGLContextObj       _cgl_ctx;       // current context at time of texture creation
+	GLuint              _texName;
+	NSAttributedString  *_string;
+	NSColor             *_color, *_borderColor;
+	NSSize              _texSize;
+	NSSize              _marginSize;    // offset default is 4 width 2 height
+	NSSize              _frameSize;
 }
 
-- (id)initWithAttributedString:(NSAttributedString*)attributedString;
-- (id)initWithAttributedString:(NSAttributedString*)attributedString andBackgroundColor:(NSColor*)color;
+- (instancetype)initWithAttributedString:(NSAttributedString*)attributedString;
+- (instancetype)initWithAttributedString:(NSAttributedString*)attributedString andBackgroundColor:(NSColor*)color;
 
 - (void)setString:(NSAttributedString *)attributedString;
 - (void)setString:(NSString *)string withAttributes:(NSDictionary*)attrs;
